79e4ca407a2db8c184e665cd0571b147be78ce67
Jethro Ordaneza <jet.oneza@gmail.com>
2023-08-28 10:06:55 +0800

    feat(home): serve static home page

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
+	app.Static("/", "./internal/server/http/web/templates/index.html")
+
+	app.Get("/api", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
