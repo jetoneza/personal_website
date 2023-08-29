a877a63a02ed2f8a92550f3b21fc763fe229f440
Jethro Ordaneza <jet.oneza@gmail.com>
2023-08-29 21:20:47 +0800

    feat: integrate tailwindcss

--- internal/template/src/routes/+page.svelte
+++ internal/template/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container mx-auto px-4 py-10">
+  <article class="prose">
+    <h1>Hello there! It's me!</h1>
+      <p>
+        Quisque tempor nisi eget orci hendrerit commodo. Quisque ut rhoncus
+        mauris, et volutpat risus. Duis dictum vel nibh eu venenatis.
+        Suspendisse potenti. Donec nec malesuada elit, semper rutrum tellus.
+        Aenean venenatis odio purus, ut cursus ipsum ultrices ut. Quisque
+        viverra consectetur interdum. Suspendisse ac luctus elit. Duis a
+        felis vestibulum purus suscipit suscipit quis id ante. Nullam ac
+        commodo felis, eget ornare ipsum. Orci varius natoque penatibus et
+        magnis dis parturient montes, nascetur ridiculus mus. Aenean tellus
+        odio, luctus vitae bibendum fringilla, sagittis molestie odio.
+      </p>
+      <button class="bg-blue-700 text-white px-4 py-2 rounded-lg font-bold">Button</button>
+  </article>
+</div>
--- internal/template/svelte.config.js
+++ internal/template/svelte.config.js
@@ -5,17 +5,17 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess({})],
 
-  kit: {
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: "index.html",
-      precompress: false,
-      strict: true,
-    })
-  }
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true,
+		}),
+	},
 };
 
 export default config;
