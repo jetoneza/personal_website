552d3f044ae6dcae43289e43a8595f352dc11ceb
Jethro Ordaneza <jet.oneza@gmail.com>
2023-08-29 19:06:24 +0800

    refactor: put main.go back to root directory

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
-	app.Static("/", "../../internal/server/http/web/templates/index.html")
-
 	app.Get("/api", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
