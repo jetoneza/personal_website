ddbb7dad63c9a201b0ed2d9edea063c04505b18e
Jethro Ordaneza <jet.oneza@gmail.com>
2023-08-29 19:37:22 +0800

    feat: add logger

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import "github.com/gofiber/fiber/v2"
+import "github.com/gofiber/fiber/v2/middleware/logger"
 
 const (
 	appName = "Jet Ordaneza Personal Website"
@@ -12,6 +13,11 @@ func main() {
 		AppName: appName,
 	})
 	defer app.Shutdown()
+
+  // Middlewares
+  app.Use(logger.New(logger.Config{
+    Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+  }))
 
 	// Go!
 	if err := app.Listen(port); err != nil {
