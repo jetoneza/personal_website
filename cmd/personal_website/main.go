19055c92c048db0de0fe497e8ef429507c112d5a
Jethro Ordaneza <jet.oneza@gmail.com>
2023-08-29 18:16:49 +0800

    refactor: create cmd folder and add main

--- cmd/personal_website/main.go
+++ cmd/personal_website/main.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
-	app.Static("/", "./internal/server/http/web/templates/index.html")
+	app.Static("/", "../../internal/server/http/web/templates/index.html")
 
 	app.Get("/api", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
