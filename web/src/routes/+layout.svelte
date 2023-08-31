24d75cebc08e8112080f35aa4ce76b59c741300f
Jethro Ordaneza <jet.oneza@gmail.com>
2023-08-31 12:59:00 +0800

    feat: implement initial layout

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -2,4 +2,19 @@
 	import '../app.postcss';
 </script>
 
+<nav class="py-6 px-10">
+	<ul class="flex space-x-10 font-mono justify-end">
+		<li><a href="/">Home</a></li>
+		<li><a href="/blog">Blog</a></li>
+		<li><a href="/about">About</a></li>
+		<li><a href="/contact">Contact</a></li>
+	</ul>
+</nav>
+
 <slot />
+
+<style lang="postcss">
+	li {
+		@apply hover:font-bold;
+	}
+</style>
