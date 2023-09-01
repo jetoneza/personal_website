4427754863b69953aad10e4e4f1a586982494868
Jethro Ordaneza <jet.oneza@gmail.com>
2023-09-01 20:52:49 +0800

    feat: create own component for navigation

---
+++ web/src/lib/components/Nav.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<nav class="main-nav flex pt-6 pb-4 px-10 justify-between border-b">
+	<a href="/" class="text-2xl font-serif"
+		><span class="font-bold hover:text-cyan-600">Jet</span> Ordaneza</a
+	>
+	<ul class="flex space-x-10 font-mono justify-end">
+		<!-- TODO: Show links if content is available -->
+		<li aria-current={$page.url.pathname === '/blog' ? 'page' : undefined}>
+			<a href="/blog">Blog</a>
+		</li>
+		<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+			<a href="/about">About</a>
+		</li>
+		<li aria-current={$page.url.pathname === '/contact' ? 'page' : undefined}>
+			<a href="/contact">Contact</a>
+		</li>
+	</ul>
+</nav>
+
+<style lang="postcss">
+	li {
+		@apply hover:font-bold hover:text-cyan-600;
+	}
+
+	li[aria-current='page'] {
+		@apply font-bold text-cyan-600;
+	}
+
+	.main-nav {
+		view-transition-name: header;
+	}
+</style>
