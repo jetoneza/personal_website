02a2bf8dd3aa2e39add04f4b8e243d13a6405012
Jethro Ordaneza <jet.oneza@gmail.com>
2023-09-02 12:43:36 +0800

    feat: integrate hamburger menu for mobile

--- web/src/lib/components/Nav.svelte
+++ web/src/lib/components/Nav.svelte
@@ -1,24 +1,52 @@
 <script lang="ts">
+	import { afterNavigate } from '$app/navigation';
 	import { page } from '$app/stores';
+	import HamburgerMenu from './HamburgerMenu.svelte';
+
+	let open = false;
+
+	afterNavigate(() => {
+		open = false;
+	});
+
+	function toggleMenu() {
+		open = !open;
+	}
 </script>
 
-<nav class="main-nav flex pt-6 pb-4 px-10 justify-between border-b">
+<nav class="main-nav flex p-4 sm:pt-6 sm:px-10 justify-between border-b">
 	<a href="/" class="text-2xl font-serif"
 		><span class="font-bold hover:text-cyan-600">Jet</span> Ordaneza</a
 	>
-	<ul class="flex space-x-10 font-mono justify-end">
+
+	<HamburgerMenu {open} onClick={toggleMenu} />
+
+	<ul
+		class="{open ? 'flex' : 'hidden'} absolute bg-white top-20 left-0 right-0
+    bottom-0 px-4 space-y-6 flex-col sm:p-0 sm:top-0 sm:space-y-0 sm:relative
+    sm:flex sm:flex-row sm:space-x-10 font-mono sm:justify-end items-center"
+	>
 		<!-- TODO: Show links if content is available -->
-		<!-- <li aria-current={$page.url.pathname === '/blog' ? 'page' : undefined}> -->
-		<!-- 	<a href="/blog">Blog</a> -->
-		<!-- </li> -->
-		<!-- <li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}> -->
-		<!-- 	<a href="/about">About</a> -->
-		<!-- </li> -->
-		<li aria-current={$page.url.pathname === '/contact' ? 'page' : undefined}>
+		<li
+			class="text-lg sm:text-base"
+			aria-current={$page.url.pathname === '/blog' ? 'page' : undefined}
+		>
+			<a href="/blog">Blog</a>
+		</li>
+		<li
+			class="text-lg sm:text-base"
+			aria-current={$page.url.pathname === '/about' ? 'page' : undefined}
+		>
+			<a href="/about">About</a>
+		</li>
+		<li
+			class="text-lg sm:text-base"
+			aria-current={$page.url.pathname === '/contact' ? 'page' : undefined}
+		>
 			<a href="/contact">Contact</a>
 		</li>
 		<li>
-			<a href="https://github.com/jetoneza" target="_blank">Github</a>
+			<a class="text-lg sm:text-base" href="https://github.com/jetoneza" target="_blank">Github</a>
 		</li>
 	</ul>
 </nav>
