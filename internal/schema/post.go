46dc2418dd7dd6f1118b77207abcf4c990c942c0
Jethro Ordaneza <jet.oneza@gmail.com>
2023-09-09 12:37:29 +0800

    feat: add create post schema

---
+++ internal/schema/post.go
@@ -0,0 +1,8 @@
+package schema
+
+type CreatePostSchema struct {
+	Title     string `json:"title" validate:"required"`
+	Content   string `json:"content" validate:"required"`
+	Category  string `json:"category,omitempty"`
+	Published bool   `json:"published,omitempty"`
+}
