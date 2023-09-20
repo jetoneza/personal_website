ae108df976bd8384cff8a42c04e03fa68a8d85d8
Jethro Ordaneza <jet.oneza@gmail.com>
2023-09-20 15:49:26 +0800

    refactor: remove the markdown preprocessor for svelte

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,18 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { mdsvex } from 'mdsvex';
-
-/** @type {import('mdsvex').MdsvexOptions} */
-const mdsvexOptions = {
-	extensions: ['.md'],
-};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	extensions: ['.svelte', '.md'],
-	preprocess: [vitePreprocess({}), mdsvex(mdsvexOptions)],
+	extensions: ['.svelte'],
+	preprocess: [vitePreprocess({})],
 
 	kit: {
 		adapter: adapter({
