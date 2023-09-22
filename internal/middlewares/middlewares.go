92cd63dea2da6ec72774970beb7078d927de95c7
Jethro Ordaneza <jet.oneza@gmail.com>
2023-09-22 20:57:29 +0800

    feat: install global middlewares

--- internal/middlewares/middlewares.go
+++ internal/middlewares/middlewares.go
@@ -2,11 +2,21 @@ package middlewares
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/etag"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
 
 func SetupMiddlewares(app *fiber.App) {
+	app.Use(compress.New())
+	app.Use(etag.New())
+	app.Use(recover.New())
+	app.Use(requestid.New())
+
 	app.Use(logger.New(logger.Config{
 		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
 	}))
@@ -14,5 +24,15 @@ func SetupMiddlewares(app *fiber.App) {
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "*",
 		AllowHeaders: "*",
+	}))
+
+	app.Use(limiter.New(limiter.Config{
+		Max: 100,
+		LimitReached: func(ctx *fiber.Ctx) error {
+			return ctx.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{
+				"status": "fail",
+				"error":  "Request limit exceeded.",
+			})
+		},
 	}))
 }
