14fe2b37d985f463075a829beef5e135fd8e2b3f
Jethro Ordaneza <jet.oneza@gmail.com>
2023-09-22 20:12:18 +0800

    refactor: use a standard claims structure for jwt

--- pkg/utils/jwt.go
+++ pkg/utils/jwt.go
@@ -12,22 +12,37 @@ type AuthTokenPayload struct {
 	Email string
 }
 
-func GenerateAuthToken(payload *AuthTokenPayload) string {
+type AuthClaims struct {
+	ID    string `json:"id"`
+	Email string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+func GenerateAuthToken(payload *AuthTokenPayload) (string, time.Time) {
 	duration, err := time.ParseDuration(config.TOKEN_EXPIRY)
 	if err != nil {
 		panic("Invalid time duration.")
 	}
 
-	t := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"exp":   time.Now().Add(duration).Unix(),
-		"id":    payload.ID,
-		"email": payload.Email,
-	})
+	expiry := time.Now().Add(duration)
+
+	claims := AuthClaims{
+		payload.ID,
+		payload.Email,
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expiry),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			Issuer:    config.APP_NAME,
+		},
+	}
+
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	token, err := t.SignedString([]byte(config.TOKEN_KEY))
 	if err != nil {
 		panic(err)
 	}
 
-	return token
+	return token, expiry
 }
