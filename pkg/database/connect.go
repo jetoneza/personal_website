2144cef2800110533611172678ccd124d85c46e9
Jethro Ordaneza <jet.oneza@gmail.com>
2023-09-22 12:01:36 +0800

    feat: add db logger

--- pkg/database/connect.go
+++ pkg/database/connect.go
@@ -3,17 +3,22 @@ package database
 import (
 	"errors"
 	"log"
+	"time"
 
 	"github.com/jetoneza/personal_website/internal/models"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func Connect(name string) (*gorm.DB, error) {
-  // This is for sqlite only
+	// This is for sqlite only
 	dbPath := "db/"
 
-	db, err := gorm.Open(sqlite.Open(dbPath+name), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open(dbPath+name), &gorm.Config{
+		NowFunc: func() time.Time { return time.Now().Local() },
+		Logger:  logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		return nil, errors.New("Failed opening connection to sqlite:" + err.Error())
 	}
