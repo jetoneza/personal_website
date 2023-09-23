06a4063df0959c79fa2e812adee99fb309138c85
Jethro Ordaneza <jet.oneza@gmail.com>
2023-09-23 12:23:38 +0800

    build: add SVELTE_PORT env variable

--- pkg/config/env.go
+++ pkg/config/env.go
@@ -11,13 +11,13 @@ var (
 	APP_NAME     = getEnv("APP_NAME", "Jet Ordaneza Personal Website")
 	DB           = getEnv("DB", "jetdb.sqlite")
 	PORT         = getEnv("PORT", "3000")
+	SVELTE_PORT  = getEnv("SVELTE_PORT", "3003")
 	TOKEN_KEY    = getEnv("TOKEN_KEY", "")
 	TOKEN_EXPIRY = getEnv("TOKEN_EXPIRY", "10h")
 )
 
 func getEnv(name string, fallback string) string {
 	if value, exists := os.LookupEnv(name); exists {
-		fmt.Println(value)
 		return value
 	}
 
