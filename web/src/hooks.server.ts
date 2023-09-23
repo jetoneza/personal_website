61424d223056d40083315771b2bea1ba36ceb938
Jethro Ordaneza <jet.oneza@gmail.com>
2023-09-23 16:00:50 +0800

    feat: include token checking when authenticating a route in handle hook

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -1,24 +1,23 @@
 import { dev } from '$app/environment';
 import { type Handle, type HandleFetch, error, redirect } from '@sveltejs/kit';
 import { BACKEND_URL } from '$env/static/private';
-import { HTTP_CODE_NOT_FOUND, HTTP_CODE_SEE_OTHER } from '$lib/constants';
+import { API_STATUS, HTTP_CODE_NOT_FOUND, HTTP_CODE_SEE_OTHER } from '$lib/constants';
 
 const protectedRoutes = ['/admin/dashboard'];
 
 export const handle: Handle = async ({ event, resolve }) => {
   const sessionToken = event.cookies.get('session_token');
+  const hasSessionEnded = sessionToken === 'session-ended';
 
-  if (!sessionToken && protectedRoutes.includes(event.url.pathname)) {
+  if ((hasSessionEnded || !sessionToken) && protectedRoutes.includes(event.url.pathname)) {
     throw error(HTTP_CODE_NOT_FOUND, {
       message: 'Not found',
     });
   }
 
-  if (sessionToken && event.url.pathname.includes('/login')) {
-    throw redirect(HTTP_CODE_SEE_OTHER, '/admin/dashboard')
+  if (sessionToken && !hasSessionEnded && event.url.pathname.includes('/login')) {
+    throw redirect(HTTP_CODE_SEE_OTHER, '/admin/dashboard');
   }
-
-  // TODO: fetch user info and load user to locals
 
   return resolve(event);
 };
