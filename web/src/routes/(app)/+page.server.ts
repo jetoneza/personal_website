302458d01a27acd5ccd261a9b3bc7850ae5e1c79
Jethro Ordaneza <jet.oneza@gmail.com>
2023-09-27 22:32:26 +0800

    feat: add first 3 blog post links to home page

--- web/src/routes/(app)/+page.server.ts
+++ web/src/routes/(app)/+page.server.ts
@@ -1,6 +1,27 @@
-import type { Actions } from './$types';
+import { formatDate } from '$lib/utils/date';
+import type { Actions, PageServerLoad } from './$types';
 
 const TIME_YEAR = 60 * 60 * 24 * 365;
+
+export const load: PageServerLoad = async ({ fetch, url }) => {
+  const qs = new URLSearchParams();
+  qs.set('page', '1');
+  qs.set('limit', '3');
+
+  // TODO: Add error handling
+  const res = await fetch(`/api/v1/posts?${qs.toString()}`);
+  const jsonResponse = await res.json();
+
+  // TODO: Improve formatting of dates
+  const posts = jsonResponse.data.map((post: Post) => {
+    return {
+      ...post,
+      formattedCreatedAt: formatDate(post.createdAt as string),
+    };
+  });
+
+  return { posts };
+};
 
 export const actions: Actions = {
   setTheme: async ({ url, cookies }) => {
--- web/src/routes/(app)/+page.svelte
+++ web/src/routes/(app)/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+</script>
+
 <div class="header space-y-8">
   <h1 class="name font-bold text-4xl font-sans-pro">
     Hello, I'm Jet<span class="text-cyan-600">.</span>
@@ -5,19 +11,33 @@
 
   <div class="content space-y-4">
     <p class="text-justify">
-      A <span class="font-bold">Software Engineer</span>. I'm always curious about how things work
-      and love exploring new stuff.
-    </p>
-
-    <p class="text-justify">
-      When I'm not coding, you can find me doing a few different things. I enjoy playing games,
-      riding my bicycle, and strumming my guitar. These are my go-to hobbies that help me relax and
-      have fun.
+      A <span class="font-bold">Software Engineer</span>. I love building cool things with code. I'm
+      always curious about how things work and love exploring new stuff.
     </p>
 
     <p class="text-justify">
       I'm excited to share my journey with you, whether it's my coding adventures or my interests
       outside of work. Let's explore the world together!
     </p>
+  </div>
+</div>
+
+<div class="posts mt-16">
+  <h1 class="font-bold text-2xl font-sans-pro">Latest posts</h1>
+  <ul class="space-y-4 mt-8">
+    {#each data.posts as post}
+      <li class="flex gap-10">
+        <div class="italic">
+          <time datetime={post.createdAt}>{post.formattedCreatedAt}</time>
+        </div>
+        <!-- TODO: Use slug for links -->
+        <a href="/blog/{post.slug}" class="font-bold">
+          {post.title} <br />
+        </a>
+      </li>
+    {/each}
+  </ul>
+  <div class="mt-8">
+    <a href="/blog" class="font-bold underline">See more</a>
   </div>
 </div>
