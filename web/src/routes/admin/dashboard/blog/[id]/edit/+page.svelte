2cbc96b9ad9d7add939ac750a4ccfbdc9c672bdc
Jethro Ordaneza <jet.oneza@gmail.com>
2023-09-27 18:54:12 +0800

    feat: wrap edit form with a container

--- web/src/routes/admin/dashboard/blog/[id]/edit/+page.svelte
+++ web/src/routes/admin/dashboard/blog/[id]/edit/+page.svelte
@@ -19,28 +19,32 @@
   } = data.post;
 </script>
 
-<div>
-  {#if form?.status === API_STATUS.FAIL}
-    <p class="p-4 bg-pink-100 border border-red-500 rounded-lg text-red-500 my-6 text-sm">
-      {form?.message}
-    </p>
-  {/if}
-  {#if form?.status === API_STATUS.SUCCESS}
-    <p class="p-4 bg-green-100 border border-green-700 rounded-lg text-green-700 my-6 text-sm">
-      {form?.message}
-    </p>
-  {/if}
-  <form class="w-full" method="POST" use:enhance>
-    <PostForm
-      action="Edit Post"
-      {title}
-      {slug}
-      {description}
-      {category}
-      {meta_title}
-      {meta_description}
-      {meta_keyword}
-      content={original_content}
-    />
-  </form>
+<div class="container flex flex-col mx-auto gap-8">
+  <h1 class="font-bold text-xl">Edit Post</h1>
+
+  <div>
+    {#if form?.status === API_STATUS.FAIL}
+      <p class="p-4 bg-pink-100 border border-red-500 rounded-lg text-red-500 my-6 text-sm">
+        {form?.message}
+      </p>
+    {/if}
+    {#if form?.status === API_STATUS.SUCCESS}
+      <p class="p-4 bg-green-100 border border-green-700 rounded-lg text-green-700 my-6 text-sm">
+        {form?.message}
+      </p>
+    {/if}
+    <form class="w-full" method="POST" use:enhance>
+      <PostForm
+        action="Edit Post"
+        {title}
+        {slug}
+        {description}
+        {category}
+        {meta_title}
+        {meta_description}
+        {meta_keyword}
+        content={original_content}
+      />
+    </form>
+  </div>
 </div>
