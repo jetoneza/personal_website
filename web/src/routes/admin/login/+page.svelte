85df22e448dee3f19ec58b659c3ca2c7de36d230
Jethro Ordaneza <jet.oneza@gmail.com>
2023-09-27 14:50:59 +0800

    feat: update login UI

--- web/src/routes/admin/login/+page.svelte
+++ web/src/routes/admin/login/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { enhance } from '$app/forms';
+  import Input from '$lib/components/Input.svelte';
   import { API_STATUS } from '$lib/constants';
   import type { ActionData } from './$types';
 
@@ -7,9 +8,10 @@
 </script>
 
 <div class="container login mx-auto p-6 flex flex-col justify-center items-center h-screen">
-  <div class="form-wrapper w-80 p-6 rounded-lg drop-shadow-lg bg-white dark:bg-transparent">
-    <div class="title font-bold text-lg">Login to Dashboard</div>
-
+  <div class="title font-bold text-3xl font-sans-pro">Login to Dashboard</div>
+  <div
+    class="mt-6 form-wrapper w-80 p-6 rounded-lg drop-shadow-lg bg-white dark:bg-transparent dark:border-2 dark:border-zinc-700"
+  >
     {#if form?.status === API_STATUS.FAIL}
       <p class="p-4 bg-pink-100 border border-red-500 rounded-lg text-red-500 my-2 text-sm">
         {form?.message}
@@ -17,48 +19,9 @@
     {/if}
 
     <form class="flex flex-col space-y-4 mt-4" method="POST" use:enhance>
-      <div class="input-wrapper">
-        <label for="email" class="block mb-2 text-sm font-medium dark:text-white"
-          >Email address</label
-        >
-        <input
-          type="email"
-          id="email"
-          name="email"
-          class="
-            border border-gray-300 rounded-lg text-sm block w-full p-2.5
-            focus:ring-2 focus:outline-none focus:ring-blue-300
-            dark:text-slate-800
-          "
-          required
-        />
-      </div>
-      <div class="input-wrapper">
-        <label for="password" class="block mb-2 text-sm font-medium dark:text-white">
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          class="
-            border border-gray-300 rounded-lg text-sm block w-full p-2.5
-            focus:ring-2 focus:outline-none focus:ring-blue-300
-            dark:text-slate-800
-          "
-          required
-        />
-      </div>
-      <button
-        type="submit"
-        class="
-          text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4
-          focus:outline-none focus:ring-blue-300 font-medium rounded-lg
-          text-sm w-full sm:w-auto px-5 py-2.5 text-center
-        "
-      >
-        Submit
-      </button>
+      <Input type="email" name="email" label="Email" required={true} />
+      <Input type="password" name="password" label="Password" required={true} />
+      <button type="submit" class="btn"> Submit </button>
     </form>
   </div>
 </div>
