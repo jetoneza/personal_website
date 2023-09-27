4976adc047507bc346775861b7b1cf05daf67d2d
Jethro Ordaneza <jet.oneza@gmail.com>
2023-09-27 13:08:10 +0800

    feat: add auth middleware to create post endpoint

--- internal/routes/post.routes.go
+++ internal/routes/post.routes.go
@@ -3,12 +3,13 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/jetoneza/personal_website/internal/handlers"
+	"github.com/jetoneza/personal_website/internal/middlewares"
 )
 
 func PostRoutes(router fiber.Router, handlers *handlers.Handler) {
 	r := router.Group("/posts")
 
 	r.Get("/", handlers.GetAllPosts)
-	r.Post("/", handlers.CreatePost)
+	r.Post("/", middlewares.AuthMiddleware(), handlers.CreatePost)
 	r.Get("/:id", handlers.GetPost)
 }
