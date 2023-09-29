b593e022f009d5e4d7d428701750c61d288e5469
Jethro Ordaneza <jet.oneza@gmail.com>
2023-09-29 14:25:55 +0800

    feat: add initial content to dashboard page

--- web/src/routes/admin/dashboard/+page.svelte
+++ web/src/routes/admin/dashboard/+page.svelte
@@ -1,1 +1,15 @@
-<h1>Dashboard Home</h1>
+<main class="blog-dashboard">
+  <div class="actions w-full flex justify-between items-center">
+    <h1 class="font-bold text-2xl font-sans-pro">Dashboard</h1>
+  </div>
+
+  <div class="max-w-sm mt-10 space-y-3 p-6 bg-white border border-zinc-200 rounded-lg shadow dark:bg-zinc-900 dark:border-zinc-800">
+    <h5 class="text-2xl font-bold font-sans-pro">Post for your blog</h5>
+    <p class="font-normal">Add a post to your blog and share it to your friends.</p>
+    <div class="action">
+      <a href="/admin/dashboard/blog/create" class="inline-block btn">
+        Create Post
+      </a>
+    </div>
+  </div>
+</main>
