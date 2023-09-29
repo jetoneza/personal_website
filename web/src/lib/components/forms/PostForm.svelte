678ff1d83f3d8bb0a0579c80cf1bc8ededf730da
Jethro Ordaneza <jet.oneza@gmail.com>
2023-09-29 14:08:52 +0800

    feat: add published select input in post form

--- web/src/lib/components/forms/PostForm.svelte
+++ web/src/lib/components/forms/PostForm.svelte
@@ -11,6 +11,7 @@
   export let meta_keywords = '';
   export let meta_image_url = '';
   export let content = '';
+  export let published = false;
 </script>
 
 <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
@@ -19,6 +20,22 @@
     <Input type="text" name="slug" value={slug} label="Slug" />
     <Input type="text" name="description" value={description} label="Description" />
     <Input type="text" name="category" value={category} label="Category" />
+    <div class="input-wrapper">
+      <label for="published" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >Publish Post</label
+      >
+      <select
+        value={published}
+        id="published"
+        name="published"
+        class="h-10 w-full rounded-md border border-input bg-background px-3
+        py-2 text-sm outline-none focus:outline-zinc-500 dark:bg-zinc-800 dark:border-zinc-700
+        dark:focus:outline-zinc-600"
+      >
+        <option selected value={false}>No</option>
+        <option value={true}>Yes</option>
+      </select>
+    </div>
   </div>
   <div class="flex flex-col gap-2">
     <Input type="text" name="meta_title" value={meta_title} label="Meta Title" />
