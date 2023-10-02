6a7284da4617573df04b9d4bb90b1bfd717321fe
Jethro Ordaneza <jet.oneza@gmail.com>
2023-10-02 17:27:57 +0800

    feat: add basic error page

---
+++ web/src/routes/+error.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<section class="py-[120px]">
+  <div class="container mx-auto">
+    <div class="-mx-4 flex">
+      <div class="w-full px-4">
+        <div class="mx-auto max-w-[400px] text-center">
+          <h2
+            class="mb-2 text-[50px] font-bold leading-none font-sans-pro sm:text-[80px] md:text-[100px]"
+          >
+            404
+          </h2>
+          <h4 class="mb-3 text-[22px] font-semibold leading-tight">
+            Page Not Found
+          </h4>
+          <p class="mb-8 text-lg dark:text-zinc-500">
+            Sorry! the page you are looking for could not be found.
+          </p>
+          <a
+            href="/"
+            class="btn"
+          >
+            Go To Home
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
