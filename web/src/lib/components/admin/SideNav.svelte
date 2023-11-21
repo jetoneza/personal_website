ed6e5136ebfe47ef93eba76d9c64828b57353771
Jethro Ordaneza <jet.oneza@gmail.com>
2023-11-21 16:54:02 +0800

    feat: add work link to sidebar

--- web/src/lib/components/admin/SideNav.svelte
+++ web/src/lib/components/admin/SideNav.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { page } from '$app/stores';
+
   import ThemeSwitch from '../ThemeSwitch.svelte';
 </script>
 
@@ -68,6 +69,32 @@
             </svg>
           </span>
           <span>Blog</span>
+        </a>
+      </li>
+      <li>
+        <a
+          href="/admin/dashboard/work"
+          class="flex items-center p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-zinc-600 group gap-2"
+          aria-current={$page.url.pathname === '/admin/dashboard/work' ? 'page' : undefined}
+        >
+          <span>
+            <svg
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              width="1em"
+              class="fill-gray-900 dark:fill-white"
+            >
+              <path
+                d="M19.728 10.686c-2.38 2.256-6.153 3.381-9.875 3.381-3.722 0-7.4-1.126-9.571-3.371L0 10.437V18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-7.6l-.272.286Z"
+              />
+              <path
+                d="m.135 7.847 1.542 1.417c3.6 3.712 12.747 3.7 16.635.01L19.605 7.9A.98.98 0 0 1 20 7.652V6a2 2 0 0 0-2-2h-3V3a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v1H2a2 2 0 0 0-2 2v1.765c.047.024.092.051.135.082ZM10 10.25a1.25 1.25 0 1 1 0-2.5 1.25 1.25 0 0 1 0 2.5ZM7 3a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1H7V3Z"
+              />
+            </svg>
+          </span>
+          <span>Work</span>
         </a>
       </li>
     </ul>
