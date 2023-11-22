7f0c658f7975a1b90471bbba6b4cabc547f125d3
Jethro Ordaneza <jet.oneza@gmail.com>
2023-11-22 22:14:31 +0800

    build: install flowbite components

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -6,7 +6,10 @@ const disabledCss = {
 
 /** @type {import('tailwindcss').Config}*/
 const config = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -20,10 +23,25 @@ const config = {
         xl: { css: disabledCss },
         '2xl': { css: disabledCss },
       },
+      colors: {
+        // flowbite-svelte
+        primary: {
+          50: '#FFF5F2',
+          100: '#FFF1EE',
+          200: '#FFE4DE',
+          300: '#FFD5CC',
+          400: '#FFBCAD',
+          500: '#FE795D',
+          600: '#EF562F',
+          700: '#EB4F27',
+          800: '#CC4522',
+          900: '#A5371B',
+        },
+      },
     },
   },
   darkMode: 'class',
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'), require('flowbite/plugin')],
 };
 
 module.exports = config;
