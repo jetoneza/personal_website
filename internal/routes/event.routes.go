fd0df7446b2da7cdbebe62d72b009ba4cff3361d
Jethro Ordaneza <jet.oneza@gmail.com>
2023-11-24 17:38:51 +0800

    feat: create events routes

---
+++ internal/routes/event.routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/jetoneza/personal_website/internal/handlers"
+	"github.com/jetoneza/personal_website/internal/middlewares"
+)
+
+func EventRoutes(router fiber.Router, handlers *handlers.Handler) {
+	r := router.Group("/events")
+
+	r.Get("/", middlewares.AuthMiddleware(), handlers.GetAllEvents)
+	r.Post("/", middlewares.AuthMiddleware(), handlers.CreateEvent)
+	r.Get("/:id", middlewares.AuthMiddleware(), handlers.GetEvent)
+}
