7e608086f9ad5dd9dbeae8a16d94396c1f9bd1bf
Jethro Ordaneza <jet.oneza@gmail.com>
2023-11-25 10:29:48 +0800

    feat: use year filter when requesting events in the frontend

--- web/src/routes/admin/dashboard/calendar/+page.server.ts
+++ web/src/routes/admin/dashboard/calendar/+page.server.ts
@@ -7,17 +7,9 @@ import type { Actions } from './$types';
 // Constants
 import { API_STATUS, HTTP_CODE_BAD_REQUEST, MESSAGE_EVENT_CREATION_ERROR } from '$lib/constants';
 
-export const load: PageServerLoad = async ({ request, fetch, url }) => {
-  const page = Number(url.searchParams.get('page') ?? '1');
-  const limit = Number(url.searchParams.get('limit') ?? '10');
-  const message = url.searchParams.get('message');
-
-  const qs = new URLSearchParams();
-  qs.set('page', page.toString());
-  qs.set('limit', limit.toString());
-
+export const load: PageServerLoad = async ({ request, fetch }) => {
   try {
-    const res = await fetch(`/api/v1/events?${qs.toString()}`, {
+    const res = await fetch(`/api/v1/events?filter=year`, {
       headers: {
         cookie: request.headers.get('cookie') as string,
       },
@@ -29,9 +21,9 @@ export const load: PageServerLoad = async ({ request, fetch, url }) => {
       throw jsonResponse;
     }
 
-    return { events: jsonResponse.data, message: message && JSON.parse(message) };
+    return { events: jsonResponse.data };
   } catch (_) {
-    return { events: [], message: { type: API_STATUS.FAIL, value: MESSAGE_EVENT_CREATION_ERROR } };
+    return { events: [] };
   }
 };
 
