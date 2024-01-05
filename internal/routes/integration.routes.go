179d63948d63d234d6146f625901f0e4a406c04b
Jethro Ordaneza <jet.oneza@gmail.com>
2024-01-05 12:04:48 +0800

    feat: create api route for integrations

---
+++ internal/routes/integration.routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/jetoneza/personal_website/internal/handlers"
+	"github.com/jetoneza/personal_website/internal/middlewares"
+)
+
+func IntegrationRoutes(router fiber.Router, handlers *handlers.Handler) {
+	r := router.Group("/integrations")
+
+	r.Get("/strava", middlewares.AuthMiddleware(), handlers.ProcessStravaExchangeToken)
+}
