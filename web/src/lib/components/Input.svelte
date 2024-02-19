d6bf76ef29d52934db35b3d2a58ad522d352c899
Jethro Ordaneza <jet.oneza@gmail.com>
2024-02-19 16:14:28 +0800

    fix: modal form dark theme placeholder text color

--- web/src/lib/components/Input.svelte
+++ web/src/lib/components/Input.svelte
@@ -26,7 +26,7 @@
     {placeholder}
     {value}
     class="flex h-10 w-full rounded-md border border-input bg-background px-3
-    py-2 text-sm outline-none focus:outline-zinc-500 dark:bg-zinc-800 dark:border-zinc-700
+    py-2 text-sm outline-none focus:outline-zinc-500 dark:placeholder:text-zinc-400 dark:bg-zinc-800 dark:border-zinc-700
     dark:focus:outline-zinc-600"
   />
 </div>
